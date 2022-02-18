1630e071506495b118542ae0241723dbb9bd0e2b
Olushola <olushola251@gmail.com>
2022-02-18 20:52:03 +0100

    working on the reaction item

--- src/components/reaction.tsx
+++ src/components/reaction.tsx
@@ -1,11 +1,55 @@
-import { ComponentProps, FC } from "react";
+import { ComponentProps, FC, useEffect, useState } from "react";
 
 interface ReactionTypes {
+  direction: String;
   maxExtent: String | Number;
 }
 
 const Reaction = ({ children }: ComponentProps<FC<ReactionTypes>>) => {
-  return <div>{children}</div>;
+  let [items, setItems] = useState<JSX.Element[]>([]);
+
+  let handleClick = () => {
+    let itemsCpy = [...items];
+    itemsCpy.push(<ReactionItem children={children} index={itemsCpy.length} />);
+    setItems(itemsCpy);
+  };
+
+  return (
+    <div onClick={() => handleClick()} style={{ position: "relative" }}>
+      {children}
+      {items}
+    </div>
+  );
+};
+
+let ReactionItem = ({
+  children,
+  index,
+}: ComponentProps<FC<{ index: number }>>) => {
+  let startTimeStamp: any;
+  //   let previousTimeStamp = 0;
+  let [margin, setMargin] = useState(0);
+
+  useEffect(() => {
+    requestAnimationFrame(animationStep);
+  }, []);
+
+  let animationStep = (time: number) => {
+    if (startTimeStamp == null) {
+      startTimeStamp = time;
+    }
+
+    setMargin(margin - 0.1);
+
+    let timeElapsed = time - startTimeStamp;
+
+    if (timeElapsed < 3000) {
+      console.log("TIME ELAPSED", time, index);
+      requestAnimationFrame(animationStep);
+    }
+  };
+
+  return <div style={{ position: "absolute", top: margin }}>{children}</div>;
 };
 
 export default Reaction;
