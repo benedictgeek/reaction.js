dade9b8715f36328bb7f30b8c3cec9959af84e43
Olushola <olushola251@gmail.com>
2022-02-19 17:36:31 +0100

    re organized file structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
 import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "src/components/reaction.tsx",
+  input: "src/index.tsx",
   output: [
     {
       file: "dist/index.js",
       format: "cjs",
-      exports: "named", 
+      exports: "named",
       strict: false,
     },
   ],
+
   plugins: [typescript({ objectHashIgnoreUnknownHack: false })],
   external: ["react", "react-dom", "react/jsx-runtime"],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,154 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { ComponentProps, FC, useEffect, useState } from "react";
 
-import { Reaction } from "./components/reaction";
+//TODO: add a little animation to jiggle the reaction item
+//TODO: make the movement sinusodial
 
-ReactDOM.render(
-  <React.StrictMode>
+interface ReactionTypes {
+  //TODO:   direction?: string;
+  maxExtent: number;
+  duration: number;
+}
+
+const Reaction = ({
+  children,
+  maxExtent = 300,
+  duration = 2000,
+  ...props
+}: ComponentProps<FC<ReactionTypes>>) => {
+  const itemsBreakPoint = 30;
+  let [items, setItems] = useState<JSX.Element[]>([]);
+
+  let handleClick = () => {
+    let itemsCpy = [...items];
+    if (itemsCpy.length < itemsBreakPoint) {
+      itemsCpy.push(
+        <ReactionItem
+          key={Date.now() * Math.random()}
+          children={children}
+          index={itemsCpy.length}
+          onFinish={(index) => removeItem(index)}
+          maxExtent={maxExtent}
+          duration={duration}
+          {...props}
+        />
+      );
+      setItems(itemsCpy);
+    }
+  };
+
+  let removeItem = (index?: number) => {
+    let itemsCpy = [...items];
+    if (itemsCpy.length === itemsBreakPoint - 1) {
+      setItems([]);
+    }
+  };
+
+  return (
+    <div onClick={() => handleClick()} style={{ position: "relative" }}>
+      {children}
+      {items}
+    </div>
+  );
+};
+
+let ReactionItem = ({
+  children,
+  index,
+  maxExtent,
+  duration,
+  onFinish,
+}: ComponentProps<
+  FC<{ index: number; onFinish: (index?: number) => void } & ReactionTypes>
+>) => {
+  let [margin, setMargin] = useState(0);
+  let [opacity, setOpacity] = useState(1);
+
+  let startTimeStamp: any;
+  let maxExtentStep = (maxExtent * 10) / duration;
+  let opacityStep = (maxExtentStep * 10) / duration;
+  let animationFrameId: number;
+
+  useEffect(() => {
+    animationFrameId = requestAnimationFrame(animationStep);
+
+    return () => cancelAnimationFrame(animationFrameId);
+  }, []);
+
+  let animationStep = (time: number) => {
+    if (startTimeStamp == null) {
+      startTimeStamp = time;
+    }
+
+    setMargin((_margin) => _margin - maxExtentStep);
+    setOpacity((_opacity) => _opacity - opacityStep);
+
+    let timeElapsed = time - startTimeStamp;
+
+    let computedExtent = Math.min(
+      (maxExtentStep * timeElapsed) / 10,
+      maxExtent
+    );
+
+    if (computedExtent === maxExtent) {
+      setOpacity(0);
+      onFinish(index);
+    }
+
+    if (timeElapsed < duration) {
+      animationFrameId = requestAnimationFrame(animationStep);
+    }
+  };
+
+  return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
+        position: "absolute",
+        top: margin,
+        opacity: opacity,
+        zIndex: -1,
       }}
+      onClick={(e) => e.stopPropagation()}
     >
-      <Reaction maxExtent={300} duration={2000}>
-        <span style={{ cursor: "pointer", fontSize: "30px" }}>❤️</span>
-      </Reaction>
-      <span style={{ width: "20px" }} />
-      <Reaction maxExtent={300} duration={2000}>
-        <span style={{ cursor: "pointer", fontSize: "30px" }}>🔥</span>
-      </Reaction>
-      <span style={{ width: "20px" }} />
-      <Reaction maxExtent={300} duration={2000}>
-        <span style={{ cursor: "pointer", fontSize: "30px" }}>Ok!</span>
-      </Reaction>
+      {children}
     </div>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  );
+};
+
+export { Reaction };
+export default Reaction;
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import { Reaction } from "./components/index";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <div
+//       style={{
+//         width: "100vw",
+//         height: "100vh",
+//         display: "flex",
+//         flexDirection: "row",
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Reaction maxExtent={300} duration={2000}>
+//         <span style={{ cursor: "pointer", fontSize: "30px" }}>❤️</span>
+//       </Reaction>
+//       <span style={{ width: "20px" }} />
+//       <Reaction maxExtent={300} duration={2000}>
+//         <span style={{ cursor: "pointer", fontSize: "30px" }}>🔥</span>
+//       </Reaction>
+//       <span style={{ width: "20px" }} />
+//       <Reaction maxExtent={300} duration={2000}>
+//         <span style={{ cursor: "pointer", fontSize: "30px" }}>Ok!</span>
+//       </Reaction>
+//     </div>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
