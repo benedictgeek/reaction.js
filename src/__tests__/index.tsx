ef0378ec0624800fd873fcce601a8119d2c2657d
Olushola <olushola251@gmail.com>
2022-03-01 10:15:26 +0100

    added more tests

--- src/__tests__/index.tsx
+++ src/__tests__/index.tsx
@@ -3,8 +3,7 @@ import "@testing-library/jest-dom";
 
 import { Reaction } from "../index";
 
-//test that an item animates (changed position)
-//test the break point for item clear
+//test that reaction items clear after set settimeout
 
 test("A reaction click produces a reaction element", async () => {
   let child = <span>OK!</span>;
@@ -33,4 +32,46 @@ test("Multiple reaction clicks produces same number of reaction elements", async
   });
 
   expect(reactionElements.length).toBe(numberofClicks);
+});
+
+test("A reactionElement should change postion (animate)", async () => {
+  let child = <span>OK!</span>;
+
+  render(<Reaction>{child}</Reaction>);
+  fireEvent.click(screen.getByRole("reaction"));
+  let reactionElement = null;
+  await waitFor(() => {
+    reactionElement = screen.getByRole("reactionItem");
+  });
+
+  await waitFor(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 300));
+  });
+
+  let newTop = reactionElement.style.top;
+  let newOpcaity = reactionElement.style.opacity;
+
+  newTop = newTop.slice(0, newTop.length - 2);
+  newOpcaity = newOpcaity.slice(0, newOpcaity.length - 2);
+
+  expect(Number(newTop)).toBeLessThan(0);
+  expect(Number(newOpcaity)).toBeLessThan(1);
+});
+
+test("A reaction elements should have a break point of 30", async () => {
+  let child = <span>OK!</span>;
+
+  render(<Reaction>{child}</Reaction>);
+
+  for (let i = 0; i < 35; i++) {
+    fireEvent.click(screen.getByRole("reaction"));
+  }
+
+  let reactionElements = [];
+
+  await waitFor(() => {
+    reactionElements = screen.getAllByRole("reactionItem");
+  });
+
+  expect(reactionElements.length).toBe(30);
 });
